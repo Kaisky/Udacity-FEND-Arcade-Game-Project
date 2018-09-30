51d2197062b77001872a8f8eee39495cb3e2ab44
Kai M <mirchev.kaloyan@gmail.com>
2018-09-30 00:13:04 -0500

    Added score and lives logis and added game over fuctionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 
+const score = document.querySelector('.score');
+const lives = document.querySelector('.lives');
+const playAgain = document.querySelector('.play-again');
+let scoreCount = 0;
+let livesCount = 5;
+let gameOver = false; 
+
 // Enemies our player must avoid
-const Enemy = function(horizontalPosition, vertiaclaPosition, speed) {
+const Enemy = function(horizontalPosition, vertiacalPosition, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.horizontalPosition = horizontalPosition;
-    this.vertiaclaPosition = vertiaclaPosition;
+    this.vertiacalPosition = vertiacalPosition;
     this.speed = speed;
 
     // The image/sprite for our enemies, this uses
@@ -29,59 +36,76 @@ Enemy.prototype.update = function(dt) {
     // Check for collision between player and enemies
     if (player.horizontalPosition < this.horizontalPosition + 60 &&
         player.horizontalPosition + 37 > this.horizontalPosition &&
-        player.vertiaclaPosition < this.vertiaclaPosition + 25 &&
-        30 + player.vertiaclaPosition > this.vertiaclaPosition) {
-        player.horizontalPosition = 200;
-        player.vertiaclaPosition = 380;
-    }
+        player.vertiacalPosition < this.vertiacalPosition + 25 &&
+        30 + player.vertiacalPosition > this.vertiacalPosition)
+        {
+            player.horizontalPosition = 200;
+            player.vertiacalPosition = 380;
+            if(livesCount > 1) {
+                   livesCount -- ;
+                   lives.innerHTML = `${livesCount}`;   
+            }else {
+                    document.querySelector('.congrats-score').innerHTML = `${scoreCount}`;
+                    document.querySelector('.winner').style.visibility = `visible`;
+                    document.querySelector('.congrats').style.visibility = `visible`;
+                    playAgain.focus();
+                    gameOver = true;
+                }
+        }
+                           
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.horizontalPosition, this.vertiaclaPosition);
+    ctx.drawImage(Resources.get(this.sprite), this.horizontalPosition, this.vertiacalPosition);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Player = function(horizontalPosition, vertiaclaPosition, speed) {
+const Player = function(horizontalPosition, vertiacalPosition, speed) {
     this.horizontalPosition = horizontalPosition;
-    this.vertiaclaPosition = vertiaclaPosition;
+    this.vertiacalPosition = vertiacalPosition;
     this.speed = speed;
     this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function() {
     // Prevent player from moving beyond canvas wall boundaries
-    this.vertiaclaPosition < 380 || (this.vertiaclaPosition = 380);
+    this.vertiacalPosition < 380 || (this.vertiacalPosition = 380);
     this.horizontalPosition < 400 || (this.horizontalPosition = 400);
     this.horizontalPosition > 0 || (this.horizontalPosition = 0);
 
     // Check for player reaching top of canvas and winning the game
-    if (this.vertiaclaPosition < 0) {
+    if (this.vertiacalPosition < 0) {
         this.horizontalPosition = 200;
-        this.vertiaclaPosition = 380;
+        this.vertiacalPosition = 380;
+        scoreCount += 5;
+        score.innerHTML = `${scoreCount}`;
     }
 };
 
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.horizontalPosition, this.vertiaclaPosition);
+    ctx.drawImage(Resources.get(this.sprite), this.horizontalPosition, this.vertiacalPosition);
 };
 
 Player.prototype.handleInput = function(keyPress) {
-    switch (keyPress) {
-        case 'left':
-            this.horizontalPosition -= this.speed + 50;
-            break;
-        case 'up':
-            this.vertiaclaPosition -= this.speed + 30;
-            break;
-        case 'right':
-            this.horizontalPosition += this.speed + 50;
-            break;
-        case 'down':
-            this.vertiaclaPosition += this.speed + 30;
-            break;
+    if (!gameOver){
+        switch (keyPress) {
+            case 'left':
+                this.horizontalPosition -= this.speed + 50;
+                break;
+            case 'up':
+                this.vertiacalPosition -= this.speed + 30;
+                break;
+            case 'right':
+                this.horizontalPosition += this.speed + 50;
+                break;
+            case 'down':
+                this.vertiacalPosition += this.speed + 30;
+                break;
+        }
+
     }
 };
 
@@ -111,3 +135,7 @@ document.addEventListener('keyup', event  => {
 
     player.handleInput(allowedKeys[event.keyCode]);
 });
+
+//Function that restarts the game once the Play again button is clicked
+
+playAgain.addEventListener('click', ()=> window.location.reload());
